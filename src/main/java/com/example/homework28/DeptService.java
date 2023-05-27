47f686be8b78620f7ced35ce0920009814aefbdc
Kseniaefimchenko <tommo0614@gmail.com>
2023-05-27 18:32:07 +0400

    done

---
+++ src/main/java/com/example/homework28/DeptService.java
@@ -0,0 +1,15 @@
+package com.example.homework28;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public interface DeptService {
+    Employee maxSalary(Integer dept);
+
+    Employee minSalary(Integer dept);
+
+    Collection<Employee> allDept(Integer dept);
+
+    Map<Integer, List<Employee>> allEmployeesByDept();
+}
